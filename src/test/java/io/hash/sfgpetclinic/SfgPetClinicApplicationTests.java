535d2a3001829dbf43f74e0ffb9e477f88fc33f2
Hash <hashfall00@gmail.com>
2021-11-27 20:12:08 -0300

    Starting the Pet Clinic Java Project

---
+++ src/test/java/io/hash/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hash.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
